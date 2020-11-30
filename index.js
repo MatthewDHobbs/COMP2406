63665c36361c84e4fd91aa45edddb9bd07ca3ef4
MatthewDHobbs <matt.david.hobbs@gmail.com>
2020-11-30 16:31:19 -0500

    DONT

--- index.js
+++ index.js
@@ -12,6 +12,13 @@ const io = require('socket.io')(http);
 app.use(cors());
 app.options('*', cors());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+    next();
+});
+
 const ws = require('./backend/ws');
 io.on('connect', ws.connection);
 
